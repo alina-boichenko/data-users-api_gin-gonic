09448bebb54507a6f422958b25b9c30fe64fd29c
Alina Boichenko <ilub1108@gmail.com>
2023-09-02 22:43:25 +0300

    Updated user.service.go

--- services/user.service.go
+++ services/user.service.go
@@ -2,7 +2,7 @@ package services
 
 import "github.com/alina-boichenko/data-users-api_gin-gonic.git/models"
 
-type UserServise interface {
+type UserService interface {
 	CreateUser(*models.User) error
 	GetUser(*string) (*models.User, error)
 	GetAll() ([]*models.User, error)
