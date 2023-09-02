b3bb32876f0b3c4ed5d9d58d05763effdc737060
Alina Boichenko <ilub1108@gmail.com>
2023-09-02 20:56:25 +0300

    Updated user.service.go

---
+++ services/user.service.go
@@ -0,0 +1,11 @@
+package services
+
+import "github.com/alina-boichenko/data-users-api_gin-gonic.git/models"
+
+type UserServise interface {
+	CreateUser(*models.User) error
+	GetUser(*string) (*models.User, error)
+	GetAll() ([]*models.User, error)
+	UpdateUser(*models.User) error
+	DeleteUser(*string) error
+}
