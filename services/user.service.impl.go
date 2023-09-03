ba44e0d3236da0646fe602f1194cc83228a11b04
Alina Boichenko <ilub1108@gmail.com>
2023-09-03 16:02:21 +0300

    Changed name of keys from 'name' to 'user_name'

--- services/user.service.impl.go
+++ services/user.service.impl.go
@@ -30,7 +30,7 @@ func (u *UserServiceImpl) CreateUser(user *models.User) error {
 
 func (u *UserServiceImpl) GetUser(name *string) (*models.User, error) {
 	var user *models.User
-	query := bson.D{bson.E{Key: "name", Value: name}}
+	query := bson.D{bson.E{Key: "user_name", Value: name}}
 	err := u.usercollection.FindOne(u.ctx, query).Decode(&user)
 	return user, err
 }
@@ -63,8 +63,12 @@ func (u *UserServiceImpl) GetAll() ([]*models.User, error) {
 }
 
 func (u *UserServiceImpl) UpdateUser(user *models.User) error {
-	filter := bson.D{primitive.E{Key: "name", Value: user.Name}}
-	update := bson.D{primitive.E{Key: "$set", Value: bson.D{primitive.E{Key: "name", Value: user.Name}, primitive.E{Key: "age", Value: user.Age}, primitive.E{Key: "address", Value: user.Address}}}}
+	filter := bson.D{primitive.E{Key: "user_name", Value: user.Name}}
+	update := bson.D{primitive.E{Key: "$set", Value: bson.D{
+		primitive.E{Key: "user_name", Value: user.Name}, 
+		primitive.E{Key: "user_age", Value: user.Age}, 
+		primitive.E{Key: "user_address", Value: user.Address},
+	}}}
 	result, _ := u.usercollection.UpdateOne(u.ctx, filter, update)
 	if result.MatchedCount != 1 {
 		return errors.New("no matched document found for update")
@@ -73,7 +77,7 @@ func (u *UserServiceImpl) UpdateUser(user *models.User) error {
 }
 
 func (u *UserServiceImpl) DeleteUser(name *string) error {
-	filter := bson.D{primitive.E{Key: "name", Value: name}}
+	filter := bson.D{primitive.E{Key: "user_name", Value: name}}
 	result, _ := u.usercollection.DeleteOne(u.ctx, filter)
 	if result.DeletedCount != 1 {
 		return errors.New("no matched document found for delete")
